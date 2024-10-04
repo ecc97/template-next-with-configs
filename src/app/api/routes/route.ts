3374b2522e6b6480f25eb047073cefd25b69f05b
ecc97 <carmonaedwin1123@gmail.com>
2024-10-04 13:43:46 -0500

    feat: add api route basic

---
+++ src/app/api/routes/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+
+    return NextResponse.json({
+        message: 'GET: Hello from Next.js!'
+    }, { status: 200 })
+}
+
+export async function POST(request: NextRequest) {
+
+    return NextResponse.json({
+        message: 'POST: Hello from Next.js!',
+    }, { status: 200 })
+}
